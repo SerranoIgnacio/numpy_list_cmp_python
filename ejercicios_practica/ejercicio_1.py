78d65607a884bd49b37d5efecbc3024e47978294
Ignacio Serrano <johndoe@example.com>
2022-02-22 19:08:03 -0300

    Correcciones

--- ejercicios_practica/ejercicio_1.py
+++ ejercicios_practica/ejercicio_1.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     # Realizar una funcion lambda que eleve al cuadrado
     # el número pasado como parámetro
 
-    potencia_2 = lambda x:2**x
+    potencia_2 = lambda x:x**2
     pot_3 = potencia_2(3)
     print(pot_3)
     # 2)
@@ -36,6 +36,6 @@ if __name__ == '__main__':
     # Lista de numeros
     numeros = [1, -5, 4, 3]
 
-    numeros_potencia = list(map(lambda x:2**x, numeros))
+    numeros_potencia = list(map(lambda x:x**2, numeros))
     print(numeros_potencia)
     print("terminamos")
